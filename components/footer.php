fdbf6603d197e294f27364f78b92c37e850a9bb4
bpink14 <hong14010203@gmail.com>
2024-05-02 09:08:44 +0700

    init project

---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="imgs/email-icon.png" alt="">
+         <h3>email liên hệ</h3>
+         <a href="mailto:ADmin@gmail.com">ADmin@gmail.com</a>
+         <a href="mailto:supper@gmail.com">supper@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="imgs/clock-icon.png" alt="">
+         <h3>giờ mở cửa</h3>
+         <p>Sáng: 07:00 đến 11:00</p>
+         <p>Chiều: 13:00 đến 18:00</p>
+      </div>
+
+      <div class="box">
+         <img src="imgs/map-icon.png" alt="">
+         <h3>địa chỉ</h3>
+         <a href="#">Đại học Hải Phòng.</a>
+      </div>
+
+      <div class="box">
+         <img src="imgs/phone-icon.png" alt="">
+         <h3>số điện thoại</h3>
+         <a href="tel://0984277907">0984.277.907</a>
+         <a href="tel://0584678077">0584.678.077</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>LAM HỒNG DƯƠNG</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="imgs/loading.gif" alt="">
+</div>
